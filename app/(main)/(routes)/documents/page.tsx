6220f9c5f8dd913055a023a828e20ac555eff4c6
Yameen <yameenmuhammad960@gmail.com>
2023-10-22 17:55:42 +0500

    added landing page

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      {/* <Image/> */}
+      <h1>document</h1>
+    </div>
+  );
+}
+
+export default Page;
