441afa82d08f9afdeb035a949209a5da33b2345e
Yameen <yameenmuhammad960@gmail.com>
2023-10-26 02:19:30 +0500

    final commit

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -4,16 +4,21 @@ import { Button } from "@/components/ui/button";
 
 function Footer() {
   return (
-    <div className=" flex items-center w-full p-6 bg-background dark:bg-[#1F1F1F] z-50">
-      <Logo />
-      <div className=" md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-        <Button variant="ghost" size="sm">
-          Privacy Policy
-        </Button>
-        <Button variant="ghost" size="sm">
-          Terms & Conditions
-        </Button>
+    <div className=" flex flex-col items-center justify-center w-full  bg-background dark:bg-[#1F1F1F] z-50">
+      <div className=" flex items-center w-full">
+        <Logo />
+        <div className=" md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+          <Button variant="ghost" size="sm">
+            Privacy Policy
+          </Button>
+          <Button variant="ghost" size="sm">
+            Terms & Conditions
+          </Button>
+        </div>
       </div>
+      <p className=" text-xs text-muted-foreground my-2">
+        &copy; Muhammad Yameen
+      </p>
     </div>
   );
 }
